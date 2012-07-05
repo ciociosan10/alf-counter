043ba3b6c1018957e14d90240d3729b7e7e87b8b
Chris Paul <cmpaul@gmail.com>
2012-07-05 13:44:51 -0700

    Making counter GET web script auto-increment the count value.
    Removing the PUT and POST web scripts.

--- config/alfresco/extension/templates/webscripts/com/tribloom/counter/counter.get.js
+++ config/alfresco/extension/templates/webscripts/com/tribloom/counter/counter.get.js
@@ -1,18 +1,34 @@
 <import resource="classpath:alfresco/extension/templates/webscripts/com/tribloom/counter/counter.js">
 
 function main() {
+	var value = args.value;
 	var name = url.templateArgs.name;
-	var counterNode =  getCounter(name);
+	var counterNode = getCounter(name);
 	if (counterNode == null) {
+		counterNode = countersFolder.createNode(name, "dm:counter");
+	}
+	if (counterNode.islocked || !counterNode.hasPermission("Write")) {
 		status.setCode(
-			status.STATUS_NOT_FOUND, // 404
-			"No counter found with the name " + name
+			status.STATUS_NOT_AUTHORIZED,
+			"Node is locked or you do not have permission to access it"
 		);
 		return;
 	}
-	if (counterNode.hasPermission("Read")) {
-		model.name = name;
-		model.value = counterNode.properties["dm:count"];
+	if (value != null) {
+		value = parseInt(value);
+		if (isNaN(value)) {
+			status.setCode(
+				status.STATUS_BAD_REQUEST,
+				"Invalid value: " + url.templateArgs.value
+			);
+			return;
+		}
+	} else {
+		value = 1 + counterNode.properties["dm:count"];
 	}
+	counterNode.properties["dm:count"] = value;
+	counterNode.save();
+	model.name = name;
+	model.value = value;
 }
 main();
--- test/java/com/tribloom/counter/TestCounter.java
+++ test/java/com/tribloom/counter/TestCounter.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
 
 /**
@@ -34,53 +33,48 @@ public class TestCounter extends RestAssured {
 	private static final String TEST_NAME_1 = "counter1";
 	private static final Integer TEST_VALUE_1 = new Integer(2);
 	private static final String TEST_NAME_2 = "counter2";
-	private static final Integer TEST_VALUE_2 = new Integer(0);
-	private static final String FAKE_NAME = "fakeCounter";
+	private static final Integer TEST_VALUE_2 = new Integer(1);
 	
 	protected String getUrl() {
 		return HOST + SERVICE;
 	}
 	
 	@Test
-	public void testPutInitializedCounter() {
-		Response response = given().contentType(ContentType.JSON).body("{\"value\":" + TEST_VALUE_1 + "}")
-				.auth().basic(USER, PASS).expect().statusCode(HttpStatus.SC_CREATED)
-				.when().put(getUrl() + TEST_NAME_1);
-		log.info(response.asString());
-	}
-	
-	@Test
-	public void testPutEmptyCounter() {
-		Response response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_CREATED).when().put(getUrl() + TEST_NAME_2);
-		log.info(response.asString());
-	}
-	
-	@Test
-	public void testPutDuplicateEmptyCounter() {
-		Response response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_NOT_MODIFIED).when().put(getUrl() + TEST_NAME_2);
-		log.info(response.asString());
-	}
-	
-	@Test
-	public void testPutDuplicateInitializedCounter() {
-		Response response = given().contentType(ContentType.JSON).body("{\"value\":" + TEST_VALUE_1 + "}")
-				.auth().basic(USER, PASS).expect().statusCode(HttpStatus.SC_NOT_MODIFIED)
-				.when().put(getUrl() + TEST_NAME_1);
-		log.info(response.asString());
-	}
-	
-	@Test
 	public void testGetInitializedCounter() throws Exception {
 		Response response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_1);
+				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_1 + "?value=" + TEST_VALUE_1);
 		JSONObject json = new JSONObject(response.asString());
 		log.info(json.toString(2));
-		assertTrue(json.has("name"));
-		assertTrue(json.has("value") == true);
-		assertTrue(TEST_NAME_1.equals(json.get("name")));
-		assertTrue(TEST_VALUE_1.equals(json.get("value")));
+		assertTrue(json.has("name") && json.has("value"));
+		assertEquals(TEST_NAME_1, json.get("name"));
+		assertEquals(TEST_VALUE_1, json.get("value"));
+		
+		// Verify value increments
+		response = given().auth().basic(USER, PASS).expect()
+				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_1);
+		json = new JSONObject(response.asString());
+		log.info(json.toString(2));
+		assertTrue(json.has("name") && json.has("value"));
+		assertEquals(TEST_NAME_1, json.get("name"));
+		assertEquals(TEST_VALUE_1 + 1, json.get("value"));
+		
+		// Test reset
+		response = given().auth().basic(USER, PASS).expect()
+				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_1 + "?value=" + TEST_VALUE_1);
+		json = new JSONObject(response.asString());
+		log.info(json.toString(2));
+		assertTrue(json.has("name") && json.has("value"));
+		assertEquals(TEST_NAME_1, json.get("name"));
+		assertEquals(TEST_VALUE_1, json.get("value"));
+		
+		// Verify value increments
+		response = given().auth().basic(USER, PASS).expect()
+				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_1);
+		json = new JSONObject(response.asString());
+		log.info(json.toString(2));
+		assertTrue(json.has("name") && json.has("value"));
+		assertEquals(TEST_NAME_1, json.get("name"));
+		assertEquals(TEST_VALUE_1 + 1, json.get("value"));
 	}
 	
 	@Test
@@ -89,59 +83,26 @@ public class TestCounter extends RestAssured {
 				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_2);
 		JSONObject json = new JSONObject(response.asString());
 		log.info(json.toString(2));
-		assertTrue(json.has("name"));
-		assertTrue(json.has("value") == true);
-		assertTrue(TEST_NAME_2.equals(json.get("name")));
-		assertTrue(TEST_VALUE_2.equals(json.get("value")));		
-	}
-	
-	@Test
-	public void testGetFakeCounter() {
-		Response response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_NOT_FOUND).when().get(getUrl() + FAKE_NAME);
-		log.info(response.asString());
-	}
-
-	@Test
-	public void testPostIncrementedCounter() throws Exception {
-		Integer updatedValue = TEST_VALUE_1 + 1;
-		Response response = given().contentType(ContentType.JSON)
-				.body("{\"value\":" + updatedValue + "}")
-				.auth().basic(USER, PASS).expect().statusCode(HttpStatus.SC_OK)
-				.when().post(getUrl() + TEST_NAME_1);
-		log.info(response.asString());
+		assertTrue(json.has("name") && json.has("value"));
+		assertEquals(TEST_NAME_2, json.get("name"));
+		assertEquals(TEST_VALUE_2, json.get("value"));		
+		
+		// Verify that value increments automatically
+		response = given().auth().basic(USER, PASS).expect()
+				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_2);
+		json = new JSONObject(response.asString());
+		log.info(json.toString(2));
+		assertTrue(json.has("name") && json.has("value"));
+		assertEquals(TEST_NAME_2, json.get("name"));
+		assertEquals(TEST_VALUE_2 + 1, json.get("value"));
 		
 		response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_1);
-		JSONObject json = new JSONObject(response.asString());
+				.statusCode(HttpStatus.SC_OK).when().get(getUrl() + TEST_NAME_2);
+		json = new JSONObject(response.asString());
 		log.info(json.toString(2));
-		assertTrue(json.has("name"));
-		assertTrue(json.has("value") == true);
-		assertTrue(TEST_NAME_1.equals(json.get("name")));
-		assertTrue(updatedValue.equals(json.get("value")));
-	}
-	
-	@Test
-	public void testPostEmptyCounter() {
-		Response response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_BAD_REQUEST).when()
-				.post(getUrl() + TEST_NAME_1);
-		log.info(response.asString());
-	}
-	
-	@Test
-	public void testPostFakeCounter() {
-		Response response = given().contentType(ContentType.JSON).body("{\"value\":0}")
-				.auth().basic(USER, PASS).expect().statusCode(HttpStatus.SC_NOT_FOUND)
-				.when().post(getUrl() + FAKE_NAME);
-		log.info(response.asString());
-	}
-	
-	@Test
-	public void testDeleteFakeCounter() throws Exception {
-		Response response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_NOT_FOUND).when().delete(getUrl() + FAKE_NAME);
-		log.info(response.asString());
+		assertTrue(json.has("name") && json.has("value"));
+		assertEquals(TEST_NAME_2, json.get("name"));
+		assertEquals(TEST_VALUE_2 + 2, json.get("value"));		
 	}
 	
 	@Test
@@ -153,13 +114,5 @@ public class TestCounter extends RestAssured {
 		response = given().auth().basic(USER, PASS).expect()
 				.statusCode(HttpStatus.SC_OK).when().delete(getUrl() + TEST_NAME_2);
 		log.info(response.asString());
-		
-		response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_NOT_FOUND).when().get(getUrl() + TEST_NAME_1);
-		log.info(response.asString());
-		
-		response = given().auth().basic(USER, PASS).expect()
-				.statusCode(HttpStatus.SC_NOT_FOUND).when().get(getUrl() + TEST_NAME_2);
-		log.info(response.asString());	
 	}
 }
